b7e33d73c1133b8534ff91d35bdfa16c31d71167
David Calavera <david.calavera@gmail.com>
2017-04-14 08:40:53 -0700

    Flip CSS tasks order to parse custom imports
    This will allow people to set custom imports in their css files.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -20,7 +20,7 @@ gulp.task("build-preview", ["css", "js", "hugo-preview"]);
 
 gulp.task("css", () => (
   gulp.src("./src/css/*.css")
-    .pipe(postcss([cssnext(), cssImport({from: "./src/css/main.css"})]))
+    .pipe(postcss([cssImport({from: "./src/css/main.css"}), cssnext()]))
     .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream())
 ));
