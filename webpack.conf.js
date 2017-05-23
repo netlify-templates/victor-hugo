419a7da48c4d7e6ac6ec1626369bc91ec2e5aa8a
KahWee Teng <t@kw.sg>
2017-05-23 00:16:47 -0700

    Upgraded to Webpack 2

--- webpack.conf.js
+++ webpack.conf.js
@@ -3,14 +3,14 @@ import path from "path";
 
 export default {
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file?name=/[hash].[ext]"
+        loader: "file-loader?name=/[hash].[ext]"
       },
       {test: /\.json$/, loader: "json-loader"},
       {
-        loader: "babel",
+        loader: "babel-loader",
         test: /\.js?$/,
         exclude: /node_modules/,
         query: {cacheDirectory: true}
@@ -20,7 +20,7 @@ export default {
 
   plugins: [
     new webpack.ProvidePlugin({
-      "fetch": "imports?this=>global!exports?global.fetch!whatwg-fetch"
+      "fetch": "imports-loader?this=>global!exports?global.fetch!whatwg-fetch"
     })
   ],
 
