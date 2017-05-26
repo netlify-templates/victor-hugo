f69775b9f39b69af339f7a93be611c2be327843d
Rafael Luque <rafael.luque@osoco.es>
2017-05-26 19:52:30 +0200

    Add Javascript console easter egg

---
+++ site/themes/hugo-osoco-theme/static/js/easteregg.js
@@ -0,0 +1,17 @@
+console.log('  ______     ______     ______     ______     ______    ');
+console.log(' /\\  __ \\   /\\  ___\\   /\\  __ \\   /\\  ___\\   /\\  __ \\   ');
+console.log(' \\ \\ \\/\\ \\  \\ \\___  \\  \\ \\ \\/\\ \\  \\ \\ \\____  \\ \\ \\/\\ \\  ');
+console.log('  \\ \\_____\\  \\/\\_____\\  \\ \\_____\\  \\ \\_____\\  \\ \\_____\\ ');
+console.log('   \\/_____/   \\/_____/   \\/_____/   \\/_____/   \\/_____/ ');
+
+console.log("");
+console.log("Howdy, nice to meet you!");
+console.log("");
+console.log("Sorry, not too much sexy technologies around here. Barely a simple, static site");
+console.log("generated using Go templates.");
+console.log("");
+console.log("However, if you are interested in having a direct impact on challenging");
+console.log("software projects for our customers, then join OSOCO and become part of");
+console.log("our cool team.");
+console.log("");
+console.log("Visit https://osoco.es/about#42 to learn more about us and job openings.");
