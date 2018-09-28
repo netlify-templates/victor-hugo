03aa7960f595427533be97325e2c7056f291177b
Jaydev Mahadevan <jaydevm@gmail.com>
2018-09-28 17:20:41 -0500

    Redo links to landing leaf pages and make splash reel video modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
 import Drop from 'tether-drop';
+import $ from 'cash-dom';
+
+// Create flyout menus in top navigation
 
 const options = {
   classes: 'drop-theme-arrows-bounce',
@@ -13,4 +16,19 @@ const options = {
       content: document.querySelector(`#${section}-dropdown`)
     }, options)
   );
+});
+
+// Toggle YouTube video embed modal
+
+$(document).ready(function() {
+  $('#hero-button')[0].onclick = showModal;
+
+  function showModal() {
+    $('body').append($('#modal-template').html());
+    $('#close-modal')[0].onclick = hideModal;
+  };
+
+  function hideModal() {
+    $('#open-modal').remove();
+  };
 });
