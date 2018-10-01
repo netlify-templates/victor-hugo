1955e43de0ac317f95d5ea458b6885b102a0a1ad
Jaydev Mahadevan <jaydevm@gmail.com>
2018-09-30 19:23:14 -0500

    Add pricing page

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import Drop from 'tether-drop';
 import $ from 'cash-dom';
+import noUiSlider from 'nouislider';
 
 // Create flyout menus in top navigation
 
@@ -20,7 +21,11 @@ const options = {
 
 // Toggle YouTube video embed modal
 
-$(document).ready(function() {
+function videoModal() {
+  if (!$('#hero-button').length) {
+    return;
+  }
+
   $('#hero-button')[0].onclick = showModal;
 
   function showModal() {
@@ -31,4 +36,33 @@ $(document).ready(function() {
   function hideModal() {
     $('#open-modal').remove();
   };
+}
+
+// Create pricing slider
+
+function pricingSlider() {
+  if (!$('#pricing-slider').length) {
+    return;
+  }
+
+  noUiSlider.create($('#pricing-slider')[0], {
+    range: {
+      min: 1,
+      max: 100
+    },
+    start: 20,
+    step: 1,
+    connect: 'lower'
+  }).on('update', function(values) {
+    const val = parseInt(values[0]);
+    $('#num-students').html(val);
+
+    const numStudents = Math.max(20, val);
+    $('#total-price').html(2 * numStudents);
+  });
+}
+
+$(document).ready(function() {
+  videoModal();
+  pricingSlider();
 });
