ac87742c7f1af4de5fc21d84019b182ecb362027
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2018-11-02 00:46:16 +0530

    Upgrade Project (#128)
    **- Summary**
    - upgrade to babel 7
    - upgrade to eslint 5 (removed invalid rules too)
    - updated other dependencies
    
    **- Description for the changelog**
    
    build: update dependencies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,7 +6,7 @@ import pluginError from "plugin-error";
 import flatten from "gulp-flatten";
 import postcss from "gulp-postcss";
 import cssImport from "postcss-import";
-import cssnext from "postcss-cssnext";
+import postcssPresetEnv from "postcss-preset-env";
 import BrowserSync from "browser-sync";
 import webpack from "webpack";
 import webpackConfig from "./webpack.conf";
@@ -32,7 +32,7 @@ gulp.task("build-preview", ["css", "js", "fonts"], (cb) => buildSite(cb, hugoArg
 // Compile CSS with PostCSS
 gulp.task("css", () => (
   gulp.src("./src/css/*.css")
-    .pipe(postcss([cssImport({from: "./src/css/main.css"}), cssnext()]))
+    .pipe(postcss([cssImport({from: "./src/css/main.css"}), postcssPresetEnv()]))
     .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream())
 ));
