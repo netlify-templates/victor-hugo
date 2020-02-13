c43ab79c3a2abf5d88605c8ed2a7e29ffbe6d329
Mohan Balachandran <mohanb@gmail.com>
2020-02-12 23:52:55 -0500

    getting tailwind to work with victor-hugo
    What he said

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,20 @@
+// module.exports = {
+//   plugins: {
+//     "postcss-import": {},
+//     "postcss-preset-env": {
+//       browsers: "last 2 versions"
+//     },
+//     autoprefixer: {}
+//   }
+// };
+
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "postcss-preset-env": {
-      browsers: "last 2 versions"
-    },
-    autoprefixer: {}
-  }
+  plugins: [
+    require('postcss-import')(),
+    require('tailwindcss')('./src/css/tailwind.config.js'),
+    require('autoprefixer'),
+    require('postcss-preset-env')({
+      browsers: ['last 2 versions', '> 5%'],
+    }),
+  ],
 };
