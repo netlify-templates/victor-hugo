7eeca6d6f314d8a5c9824f81f424a3a4eac42a36
John <john@real-currents.com>
2020-08-13 08:15:27 -0400

    update install

--- src/install.R
+++ src/install.R
@@ -4,6 +4,10 @@ if (!require("blogdown")) {
     blogdown::install_hugo()
 }
 
-install.packages(c("caret","cluster","ggplot2","dplyr","gridExtra","repr","kknn",
-"hexbin","GGally","ROCR","pROC","glmnet","MASS","randomForest",
-"MLmetrics","e1071","klaR","nnet","plotly"))
+remotes::install_version('glmnet', version = '2.0-18')
+
+install.packages(c(
+    "caret","cluster","ggplot2","dplyr","gridExtra","repr","kknn",
+    "hexbin","GGally","ROCR","pROC","glmnet","MASS","randomForest",
+    "MLmetrics","e1071","klaR","nnet","plotly"
+))
