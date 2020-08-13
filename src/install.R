5d045113da420d4966f0d849503e0fd060015352
John <john@real-currents.com>
2020-08-12 23:28:29 -0400

    expand install

--- src/install.R
+++ src/install.R
@@ -3,3 +3,7 @@ if (!require("blogdown")) {
     library(blogdown)
     blogdown::install_hugo()
 }
+
+install.packages(c("caret","ggplot2","dplyr","gridExtra","repr","kknn",
+"hexbin","GGally","ROCR","pROC","glmnet","MASS","randomForest",
+"MLmetrics","e1071","klaR","nnet","cluster"))
