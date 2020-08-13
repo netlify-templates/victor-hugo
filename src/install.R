1e2bc793c61041f7de1481932082981bee7d0a5f
John <john@real-currents.com>
2020-08-13 13:56:22 -0400

    update config

--- src/install.R
+++ src/install.R
@@ -8,6 +8,6 @@ remotes::install_version('glmnet', version = '2.0-18')
 
 install.packages(c(
     "caret","cluster","ggplot2","dplyr","gridExtra","repr","kknn",
-    "hexbin","GGally","ROCR","pROC","glmnet","MASS","randomForest",
+    "hexbin","GGally","MASS","pROC", "ROCR", "randomForest",
     "MLmetrics","e1071","klaR","nnet","plotly"
 ))
