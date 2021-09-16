181ae39d46412751ef80d6dd9f61ebd28a6d39d9
Sergio <smaisidoro@gmail.com>
2021-09-16 21:27:24 +0300

    Remove comments

--- webpack.dev.js
+++ webpack.dev.js
@@ -17,15 +17,7 @@ module.exports = merge(common, {
     static: {
       directory: path.resolve(__dirname, "./dist"),
       staticOptions: {},
-      // Don't be confused with `devMiddleware.publicPath`, it is `publicPath` for static directory
-      // Can be:
-      // publicPath: ['/static-public-path-one/', '/static-public-path-two/'],
-      // publicPath: "/static-public-path/",
-      // Can be:
-      // serveIndex: {} (options for the `serveIndex` option you can find https://github.com/expressjs/serve-index)
       serveIndex: true,
-      // Can be:
-      // watch: {} (options for the `watch` option you can find https://github.com/paulmillr/chokidar)
       watch: true,
     },
 
